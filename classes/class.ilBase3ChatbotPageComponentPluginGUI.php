3be77592324a63047528411302cc87aae5d9c8f2
qualitus-dahme <Dahme@qualitus.de>
2025-10-23 13:36:11 +0200

    usage of classmap changed

--- classes/class.ilBase3ChatbotPageComponentPluginGUI.php
+++ classes/class.ilBase3ChatbotPageComponentPluginGUI.php
@@ -1,5 +1,6 @@
 <?php declare(strict_types=1);
 
+use Base3\Api\IDisplay;
 use Base3\Base3Ilias\PageComponent\AbstractPageComponentPluginGUI;
 
 /**
@@ -16,18 +17,27 @@ class ilBase3ChatbotPageComponentPluginGUI extends AbstractPageComponentPluginGU
 	}
 
 	protected function getDefaultProps(): array {
-		return ['endpoint' => ''];
+		return ['service' => ''];
 	}
 
 	protected function setFormContent(ilPropertyFormGUI $form, array $props): void {
-		$pageModuleEndpointControl = new ilTextInputGUI('Endpoint', 'endpoint');
-		$pageModuleEndpointControl->setValue($props['endpoint']);
+		$pageModuleEndpointControl = new ilTextInputGUI('Service Endpoint', 'service');
+		$pageModuleEndpointControl->setValue($props['service']);
 		$form->addItem($pageModuleEndpointControl);
 	}
 
 	protected function getPresentationHtml(array $a_properties, string $plugin_version): string {
+
+		// include client scripts
 		$this->mainTemplate->addJavaScript('components/Base3/ClientStack/assetloader/assetloader.min.js');
 
-		return '<div style="background:#eee;">BASE3 Chatbot []</div>';
+		// find display
+		$displays = $this->classmap->getInstances(['interface' => IDisplay::class, 'name' => 'chatbotdisplay']);
+		if (empty($displays)) return 'Display not found.';
+		$display = $displays[0];
+
+		// configure and output
+		$display->setData($a_properties);
+		return $display->getOutput();
 	}
 }
